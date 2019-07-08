3813aa6c88f39b3081d6c2138514666f79effd97
Victor Ferreira <vferreirati@gmail.com>
2019-07-08 20:34:58 -0300

    show days, hours, minutes and seconds

--- lib/pages/time_page.dart
+++ lib/pages/time_page.dart
@@ -6,12 +6,45 @@ class TimePage extends StatefulWidget {
 }
 
 class _TimePageState extends State<TimePage> {
+
+  String days;
+  String hours;
+  String minutes;
+  String seconds;
+
+  @override
+  void initState() {
+    super.initState();
+    _startLoop();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("Hello world!"),
+        child: Text("$days dias, $hours horas, $minutes minutos, $seconds segundos"),
       ),
     );
+  }
+
+  void _startLoop() async {
+      while(true) {
+        _getDates();
+        await Future.delayed(Duration(seconds: 1));
+      }
+  }
+
+  void _getDates() {
+    final now = DateTime.now();
+    final loveDate = DateTime.utc(2019, 12, 28, 12);
+
+    final dif = loveDate.difference(now);
+    final splitResult = dif.toString().split(":");
+    setState(() {
+      days = (dif.inHours ~/ 24).toString();
+      hours = (dif.inHours % 24).toString();
+      minutes = splitResult[1];
+      seconds = splitResult[2].substring(0, 2); 
+    });
   }
 }
